c5506353abf30e230ddea88d35ee569b41a6dcf0
Matti Tahvonen <matti@vaadin.com>
2014-11-11 19:39:24 +0200

    Added some documentation

--- src/test/java/org/example/MainPagePO.java
+++ src/test/java/org/example/MainPagePO.java
@@ -1,14 +1,21 @@
 package org.example;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 /**
  *
  */
 public class MainPagePO {
 
+    // addNew Button field in VaadinUI has id "addNew" and its client side counterpart
+    // is by convetion automatically wired here when the class is instantiated 
+    // with PageFactory
     private WebElement addNew;
 
+    // field without hints is analogue to below, but you can naturally use lots of 
+    // other selectors but element identifiers as well
+    @FindBy(id = "entryList")
     private WebElement entryList;
 
     public void clickNew() {
